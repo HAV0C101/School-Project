56c4cd2e89b2b50ea0323c1099c7eec6f1172e71
Cory <coder3d9@gmail.com>
2021-04-14 13:36:55 +1200

    WIP

--- App/src/app.tsx
+++ App/src/app.tsx
@@ -14,8 +14,8 @@ const App = ():JSX.Element => {
         ipcRenderer.invoke('get-api-data').then(async menu_data => {
             console.log(menu_data)
             let i = 0
-            let menus = []
-            for(const [key, menu_object]:[key:number, menu_object:menu] of Object.entries(menu_data)){
+            const menus:Array<menu> = []
+            for(const [key, menu_object] of Object.entries(menu_data)){
                 const complete_menu = menu_object.map((data:menu_item) => <p key={i++}>{data.name}, {data.price}</p>)
                 menus.push(complete_menu)
             }
